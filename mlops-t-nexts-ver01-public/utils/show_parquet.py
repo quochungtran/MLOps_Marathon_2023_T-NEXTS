c4a8e67122feeea8695b6337b1bf814aaa974449
Thy Nguyen <52106759+minhthy1016@users.noreply.github.com>
2023-07-03 01:24:51 +0700

    Add files via upload

---
+++ mlops-t-nexts-ver01-public/utils/show_parquet.py
@@ -0,0 +1,18 @@
+import sys
+
+import pandas as pd
+
+
+def show_parquet(path: str):
+    df = pd.read_parquet(path=path)
+    print(df)
+
+
+# Usage: python utils/show_parquet.py data/train_data/phase-1/prob-1/X_test.parquet
+
+if __name__ == "__main__":
+    if len(sys.argv) >= 2:
+        show_parquet(sys.argv[1])
+    else:
+        print("missing path")
+        exit(1)
